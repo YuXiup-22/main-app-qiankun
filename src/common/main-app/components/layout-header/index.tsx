3b55a6f70679c428999c36c4c71a32a02ceedfc2
tangyuxi <1145720110@qq.com>
2025-05-16 17:21:22 +0800

    feat:添加导航栏

--- src/common/main-app/components/layout-header/index.tsx
+++ src/common/main-app/components/layout-header/index.tsx
@@ -1,16 +1,42 @@
 import classnames from "classnames";
 import styles from "./index.module.scss";
-import { Tabs } from "antd";
+import { Tabs, Avatar, Input } from "antd";
+import {
+  SearchOutlined,
+  GithubOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 interface LayoutHeaderProps {
   className?: string | string[];
 }
-const items = Array.from({ length: 3 }).map((_, i) => {
+const innerPage = ["Home", "Application"];
+const items = innerPage.map((item, i) => {
   const id = String(i + 1);
   return {
-    label: `Tab ${id}`,
+    label: item,
     key: id,
+    closable: i > 1,
   };
 });
+// Left extra content
+const leftExtra = (
+  <div className={styles["left-extra"]}>
+    <span className={styles.logo}>LOGO</span>
+  </div>
+);
+
+// Right extra content
+const rightExtra = (
+  <div className={styles["right-extra"]}>
+    <Input
+      placeholder="Search..."
+      prefix={<SearchOutlined />}
+      className={styles.search}
+    />
+    <Avatar icon={<UserOutlined />} size={32} className={styles.avatar} />
+    <GithubOutlined className={styles.icon} />
+  </div>
+);
 export const LayoutHeader = (props: LayoutHeaderProps) => {
   const { className } = props;
   const cls = classnames(className, {
@@ -18,7 +44,16 @@ export const LayoutHeader = (props: LayoutHeaderProps) => {
   });
   return (
     <div className={cls}>
-      <Tabs items={items}></Tabs>
+      <Tabs
+        hideAdd
+        items={items}
+        style={{ width: "100%" }}
+        type="editable-card"
+        tabBarExtraContent={{
+          left: leftExtra,
+          right: rightExtra,
+        }}
+      ></Tabs>
     </div>
   );
 };
---
+++ src/common/main-app/components/layout-header/slots/logo/index.tsx
@@ -0,0 +1,8 @@
+export const HeaderLogo = ()=>{
+
+
+
+    return <div>
+        <img src="../../"></img>
+    </div>
+}
